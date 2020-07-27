6750e44df8d6ccab58d6a7ea457dd1c9f672ca09
Marcin Kapela <marcin.kapela.programista@gmail.com>
2020-07-27 19:44:55 +0200

    Complete ver.1.0. Buttons work. Error label and some errors validation completed. Some Css added also

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -1,18 +1,12 @@
 package application;
 
-import application.controller.MainController;
-import application.controller.PrimaryController;
-import application.model.GetFullGsonData;
 import application.view.ViewFactory;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
 import java.io.IOException;
-import java.util.Calendar;
 
 /**
  * JavaFX App
--- src/main/java/application/model/StaticValues.java
+++ src/main/java/application/model/StaticValues.java
@@ -4,5 +4,10 @@ import java.util.HashMap;
 
 public class StaticValues {
     public static String API_KEY = "efcffdfceae519b062a44af691b68cc8";
+    public static String firstTown = "Poznań";
+    public static String secondTown = "Nuuk";
+    public static String firstCountry = "";
+    public static String secondCountry = "";
+    public static String fxmlName = "/application/primary.fxml";
 
 }
