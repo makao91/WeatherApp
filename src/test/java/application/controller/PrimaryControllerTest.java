d1251a37ba3150a9021b4bedc4d210e967c155ad
Marcin Kapela <marcin.kapela.programista@gmail.com>
2020-08-04 21:32:08 +0200

    Add few tests for noJavaFx concerned  functions

--- src/test/java/application/controller/PrimaryControllerTest.java
+++ src/test/java/application/controller/PrimaryControllerTest.java
@@ -2,31 +2,52 @@ package application.controller;
 
 import application.model.GetFullGsonData;
 import application.model.GsonsClass.FullWeatherInformation;
-
-
-import application.model.GsonsClass.FullWrappedGson;
+import application.model.LegendData;
+import application.model.TableViewData;
+import application.model.TableViewLegendData;
 import com.google.gson.JsonSyntaxException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import org.junit.jupiter.api.Test;
-
 import org.mockito.Mock;
-
-
 import java.io.IOException;
 import java.util.List;
-
 import static org.hamcrest.MatcherAssert.assertThat;
-
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 
 class PrimaryControllerTest {
 
+
+    FullWeatherInformationStub weatherInfo = new FullWeatherInformationStub();
+    LegendData legendData = new LegendData();
+
+
+    @Mock
+    TableViewData tableViewData;
+    TableViewLegendData tableViewLegendData;
+
+    @Test
+    void checkAddingDataToLegendDataList(){
+        //given
+        ObservableList<TableViewLegendData> legendDataObservableList =  FXCollections.observableArrayList();
+        //when
+        legendDataObservableList.add(tableViewLegendData);
+        //then
+        assertThat(legendDataObservableList, hasSize(1));
+    }
+    @Test
+    void dataInLegendsShouldReturnProperGeneratedData() {
+        //given
+        //when
+        //then
+        assertThat(legendData.getLegednData().get("01d"), equalTo("bezchmurne niebo"));
+    }
     @Test
     void shoudGetTemperature() {
         //given
-        FullWeatherInformationStub weatherInfo = new FullWeatherInformationStub();
         //when
         List<FullWeatherInformation> fullWeatInf = weatherInfo.getFullWeatherInformationList();
         //then
@@ -39,5 +60,51 @@ class PrimaryControllerTest {
         //when
         //then
         assertThrows(JsonSyntaxException.class, () -> new GetFullGsonData("",""));
+        assertThrows(JsonSyntaxException.class, () -> new GetFullGsonData("Krak",""));
+    }
+    @Test
+    void shouldDownloadDataFromServerForProperTownName() throws IOException {
+        //given
+        GetFullGsonData getFullGsonData = new GetFullGsonData("Kraków","");
+        //when
+        //then
+        assertThat(getFullGsonData.getFullWeatherInformation(), hasSize(40));
+    }
+    @Test
+    void checkClearingfFunctionsWorkProperly() {
+        //given
+        List<FullWeatherInformation> fullWeatInf = weatherInfo.getFullWeatherInformationList();
+        ObservableList<TableViewData> dataFirstFirst = FXCollections.observableArrayList();
+        dataFirstFirst.add(tableViewData);
+        //when
+        dataFirstFirst.clear();
+        fullWeatInf.clear();
+        //then
+        assertAll(
+                () -> assertThat(dataFirstFirst, empty()),
+                () -> assertThat(fullWeatInf, empty())
+        );
+    }
+    @Test
+    void addingToListsShoudReturnListsWithElements() {
+        //given
+        List<FullWeatherInformation> fullWeatInf = weatherInfo.getFullWeatherInformationList();
+        ObservableList<TableViewData> dataFirstFirst = FXCollections.observableArrayList();
+        dataFirstFirst.add(tableViewData);
+        //when
+        //then
+        assertAll(
+                () -> assertThat(dataFirstFirst, hasItem(tableViewData)),
+                () -> assertThat(fullWeatInf, hasSize(1))
+        );
+    }
+    @Test
+    void checkValidFieldsFunction(){
+        //given
+        PrimaryController primaryController = mock(PrimaryController.class);
+        //when
+        //then
+        assertTrue(primaryController.validFields("lubie", "placki"));
+
     }
 }
